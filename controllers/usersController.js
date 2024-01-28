e115845cca1f3562d505ddf528f32def62c47a28
akorede <emiolasaheed1@gmail.com>
2024-01-28 10:46:32 +0100

    Create controller to register user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,27 @@
+import { validationResult } from "express-validator";
+import { handleError } from "../utils/errorUtil";
+import User from "../models/userModel";
+import bcrypt from 'bcryptjs';
+
 export const register = async (req, res, next) => {
-  const { name, email password } = req.body;
-  console.log(req);
+  const { name, email, password } = req.body;
+  const errors = validationResult(req);
+  try {
+    if (!errors.isEmpty()) {
+      handleError(422, errors.array[0].msg)
+    }
+    const user = await User.findOne({ where: { email } });
+
+    if (user) {
+      handleError(422, 'User already exists');
+    }
+    const newUser = await User.create({ 
+      name, 
+      email, 
+      password: await bcrypt.hash(password, 12)
+    });
+    res.status(201).send({message: 'User successfully created', user: newUser});
+  } catch (error) {
+    next(error);
+  }  
 }
--- models/userModel.js
+++ models/userModel.js
@@ -9,6 +9,7 @@ const User = sequelize.define('User', {
   email: {
     type: DataTypes.STRING,
     allowNull: false,
+    unique: true,
     validate: {
       isEmail: {
         msg: 'Please enter a valid email'
