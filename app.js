eb8f9a23f73aef4b5fb64aa1ed4179d57204c4b9
akorede <emiolasaheed1@gmail.com>
2024-01-29 19:31:42 +0100

    Set up auth route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import sequelize from './config/database.js';
 import registrationRouter from './routes/userRoutes.js';
+import authrouter from './routes/authRoutes.js';
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/v1', registrationRouter);
+app.use('/api/v1', authrouter);
 
 app.use((error, req, res, next) => {
   const message = error.message;
---
+++ routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { login } from '../controllers/authController.js';
+import { check } from 'express-validator';
+
+const authrouter = express.Router();
+
+Authrouter.post('/login', [
+  check('email').notEmpty().withMessage('Email field cannot be empty').isEmail().withMessage('Provide a valid email'),
+  check('password').notEmpty().withMessage('Password field cannot be empty')
+], login);
+
+export default authrouter;
