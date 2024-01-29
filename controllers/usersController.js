0229ae5f16f240269eac009436bf6da6d67a8a78
akorede <emiolasaheed1@gmail.com>
2024-01-29 18:16:16 +0100

    Set cookies upon user registration

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,6 +2,7 @@ import { validationResult } from "express-validator";
 import { handleError } from "../utils/errorUtil.js";
 import User from "../models/userModel.js";
 import bcrypt from 'bcryptjs';
+import jwt  from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
   console.log(req.body);
@@ -21,6 +22,14 @@ export const register = async (req, res, next) => {
       email, 
       password: await bcrypt.hash(password, 12)
     });
+    const token = jwt.sign({ userId: newUser.id }, process.env.JWT_SECRET_TOKEN, {
+      expiresIn: "1h"
+    });
+    res.cookie('auth_token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 86400000
+    })
     res.status(201).send({message: 'User successfully created', user: newUser});
   } catch (error) {
     next(error);
