c817a7715ebbb5d0b5c80a7c5b72e0bec7ea5ab2
akorede <emiolasaheed1@gmail.com>
2024-02-04 23:02:47 +0100

    Decremet feed's claps counter upon clap destoy

--- controllers/clapController.js
+++ controllers/clapController.js
@@ -25,11 +25,13 @@ export const create = async (req, res, next) => {
 export const destroy = async (req, res, next) => {
   try {
     const { feedId, userId } = req.params;
+    const feed = await Feed.findByPk(feedId);
     const clap = await Clap.findOne({ where: { UserId: userId, FeedId: feedId } })
     if (!clap) {
       handleError(404, 'No clap');
     }
     await clap.destroy();
+    await feed.decrement('claps');
     res.status(201).send({message: 'like destroyed'})
 
   } catch (error) {
